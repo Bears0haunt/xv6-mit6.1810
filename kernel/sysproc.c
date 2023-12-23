df4b3b9c81f1d9eccf17181d9c5498ba57d8112c
Changming Xiao <bearshaunt0@gmail.com>
2023-12-23 15:47:14 +0800

    Revert "Syscall"

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -91,12 +91,3 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
-
-uint64
-sys_trace(void)
-{
-  int trace_mask;
-  argint(0,&trace_mask);
-  myproc()->tracemask = trace_mask;
-  return 0;
-}
